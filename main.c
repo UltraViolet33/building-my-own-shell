786fe57fcf71f0c4d0139a4f08d7de2533f97395
Ulysse28 <uvaldenaire@gmail.com>
2023-10-14 16:40:42 +0200

    add lauch function to lauch the command

--- main.c
+++ main.c
@@ -2,71 +2,90 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define LSH_RL_BUFSIZE 1024
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define RL_BUFSIZE 1024
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
 
-void lsh_loop(void);
-char *readline(void);
-
-char **splitline(char *line);
+void main_loop(void);
+char *read_line(void);
+char **split_line(char *line);
+int launch(char **args);
 
 int main(int argc, char **argv)
 {
-    lsh_loop();
+    main_loop();
 
     return EXIT_SUCCESS;
 }
 
-
-char **splitline(char *line)
-{
-    char *token;
-    char **tokens = malloc(sizeof(char *) * BUFSIZ);
-
-    int position = 0;
-
-    token = strtok(line, LSH_TOK_DELIM);
-
-    while(token != NULL)
-    {
-        printf("%s\n", token);
-        tokens[position] = token;
-        token = strtok (NULL, LSH_TOK_DELIM);
-        position++;
-    }
-
-    return tokens;
-}
-
-
-void lsh_loop(void)
+void main_loop(void)
 {
     char *line;
     char **args;
 
-    line = readline();
-    
-    args = splitline(line);
+    line = read_line();
 
-    for (int i = 0; i < 5; i++)
-    {
-        // printf("%c", args[i]);
-    }
+    args = split_line(line);
 
+    launch(args);
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%s", args[i]);
+    // }
 }
 
-
-char *readline(void)
+char **split_line(char *line)
 {
-    int buffer_size = LSH_RL_BUFSIZE;
+    int bufsize = TOK_BUFSIZE;
+    char *token;
+    char **tokens = malloc(sizeof(char *) * bufsize);
+    int position = 0;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "Allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOK_DELIM);
+
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+
+            if (!tokens)
+            {
+                fprintf(stderr, "Allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+
+    return tokens;
+}
+
+char *read_line(void)
+{
+    int buffer_size = RL_BUFSIZE;
     int position = 0;
     char c;
 
     char *buffer = malloc(sizeof(char) * buffer_size);
-
-    printf("%d", buffer_size);
 
     if (buffer == NULL)
     {
@@ -81,7 +100,6 @@ char *readline(void)
         if (c == EOF || c == '\n')
         {
             buffer[position] = '\0';
-            printf("terminated");
             return buffer;
         }
         else
@@ -93,7 +111,7 @@ char *readline(void)
 
         if (position >= buffer_size)
         {
-            buffer_size += LSH_RL_BUFSIZE;
+            buffer_size += RL_BUFSIZE;
             buffer = realloc(buffer, buffer_size);
 
             if (buffer == NULL)
@@ -103,4 +121,38 @@ char *readline(void)
             }
         }
     }
+}
+
+int launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        // Child process
+        if (execvp(args[0], args) == -1)
+        {
+            perror("lsh");
+        }
+
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        // Error forking
+        perror("lsh");
+    }
+    else
+    {
+        // Parent process
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
